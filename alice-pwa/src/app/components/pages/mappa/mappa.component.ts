08f3570ef968930c1734ba85f4cf410326def87e
Daniele <d.tarini@hyperborea.com>
2021-05-02 12:08:43 +0200

    Map with cat icon for places

--- alice-pwa/src/app/components/pages/mappa/mappa.component.ts
+++ alice-pwa/src/app/components/pages/mappa/mappa.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// openlayers
+import Map from 'ol/Map';
+import View from 'ol/View';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorLayer from 'ol/layer/Vector' ;
+import VectorSource from 'ol/source/Vector' ;
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
 @Component({
   selector: 'app-mappa',
   templateUrl: './mappa.component.html',
@@ -7,9 +20,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MappaComponent implements OnInit {
 
+  position: any;
+  map: Map;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.position = position;
+        this.startOlMap();
+      });
+    } else {
+      this.position = null;
+    }
+  }
+
+  startOlMap() {
+    this.map = new Map({
+      target: 'olmap',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([this.position.coords.longitude, this.position.coords.latitude]),
+        zoom: 13
+      })
+    });
+    let layer = new VectorLayer({
+      source: new VectorSource({
+        features: [
+          new Feature({
+            geometry: new Point(olProj.fromLonLat([this.position.coords.longitude, this.position.coords.latitude]))
+          })
+        ]
+      }),
+      style: new Style({
+        image: new Icon({
+          anchor: [0.5, 0.5],
+          src: './assets/svg/cat.svg',
+        })
+      }),
+    });
+    this.map.addLayer(layer);
   }
 
 }
