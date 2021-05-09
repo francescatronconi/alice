29adc5d473d97a45701ec4ba5f9bb76c68e93c63
Daniele <d.tarini@hyperborea.com>
2021-05-09 15:10:36 +0200

    Audio video

--- alice-pwa/src/app/components/pages/tell-story/tell-story.component.ts
+++ alice-pwa/src/app/components/pages/tell-story/tell-story.component.ts
@@ -11,17 +11,21 @@ export class TellStoryComponent implements OnInit {
 
   chapter: StoryChapter;
   key: string;
+  chapters: {[key:string]: StoryChapter};
 
   constructor(
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
+    this.chapters = {
+      video: {title: 'Daniele e il naso', video: 'https://www.dantar.it/resources/agora/resources/daniele-tarini.mp4'},
+      audio: {title: 'Pentatonix God bless you merry gentlemen', audio: 'https://www.dantar.it/resources/agora/resources/merry.mp3'},
+    };
     this.route.params.subscribe(params => {
       this.key = params.key;
-      this.chapter = {title: 'The chapter title', audio: 'https://www.dantar.it/resources/agora/resources/merry.mp3'};
+      this.chapter = this.chapters[this.key];
     });
-
   }
 
 }
