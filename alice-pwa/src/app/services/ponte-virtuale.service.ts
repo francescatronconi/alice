98bcc584b9c3eb24a70cb90af80476d3a936ed20
Daniele <d.tarini@hyperborea.com>
2021-05-25 19:58:28 +0200

    Map Gioca button look

--- alice-pwa/src/app/services/ponte-virtuale.service.ts
+++ alice-pwa/src/app/services/ponte-virtuale.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
