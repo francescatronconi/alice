9d5d62d4b3ae256f031a82b4f735547ded434836
Daniele <d.tarini@hyperborea.com>
2021-09-19 13:03:24 +0200

    Final teaser

--- alice-pwa/src/environments/environment.prod.ts
+++ alice-pwa/src/environments/environment.prod.ts
@@ -3,4 +3,9 @@ export const environment = {
   gameUrl: './assets/trailer',
   nearby: 1000,
   gaMeasurementId: 'G-QK32TSKP6N', // alice
+  privacy: {
+    site: 'https://alice.vivavoce.it',
+    titolare: 'Associazione VivaVoce',
+    mail: 'info@vivavoce.it'
+  }  
 };
