8dd103162890dfacbbed21a2680d4471ad0d4dda
Daniele <d.tarini@hyperborea.com>
2021-09-25 03:36:21 +0200

    Selfie

--- alice-pwa/src/app/camponents/pages/camera/camera.component.ts
+++ alice-pwa/src/app/camponents/pages/camera/camera.component.ts
@@ -15,15 +15,25 @@ export class CameraComponent implements OnInit, AfterViewInit {
   @ViewChild("alice") alice: ElementRef;
 
   jpg: any;
+  clicked: boolean;
 
   constructor() { }
 
   ngAfterViewInit(): void {
+    this.clicked = false;
     console.log(this.capture);
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      console.log("yes!");
       navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
         this.capture.nativeElement.srcObject = stream;
+
+        let streamh = stream.getVideoTracks()[0].getSettings().height;
+        let captureh = this.capture.nativeElement.clientHeight;
+        console.log (captureh, streamh);
+        let img = this.alice.nativeElement as HTMLImageElement;
+        let cam = this.capture.nativeElement as HTMLVideoElement;
+        console.log(cam, cam.clientHeight, cam.clientWidth);
+        //img.width = Math.min(cam.clientHeight, cam.clientWidth);
+        img.width = img.width * captureh / streamh;
       });
     }
   }
@@ -32,7 +42,7 @@ export class CameraComponent implements OnInit, AfterViewInit {
   }
 
   clickVideo(event: any) {
-    console.log(event);
+    this.clicked = true;
     let selfie = document.createElement('canvas');
     let video = this.capture.nativeElement;
     selfie.width = video.videoWidth;
